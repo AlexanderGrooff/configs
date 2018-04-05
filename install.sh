0d51287359aaf2f91a2d9bf5f5319ff4218e7649
Alexander Grooff <alexandergrooff@Alexanders-MacBook-Air.local>
2018-04-05 23:26:21 +0200

    Added iTerm2 scripts

--- install.sh
+++ install.sh
@@ -1,1 +1,28 @@
-echo hi
+#!/usr/bin/env bash
+
+usage() { echo "Usage: $0 [-p <iterm2,>]" 1>&2; exit 1; }
+
+while getopts ":p:" o; do
+    case "${o}" in
+        p)
+            	p=${OPTARG}
+            	((p == 'iterm2')) || usage
+		echo "Installing iTerm2 config"
+		echo "Installing ohmyzsh"
+		./iterm2/install_oh_my_zsh.sh
+		echo "Setting ZSH theme in ~/.zshrc to 'agnoster'"
+		perl -pi -e 's/ZSH_THEME=\"\w+\"/ZSH_THEME="agnostic"/g' ~/.zshrc
+		echo "Installing zsh highlighting"
+		brew install zsh-syntax-highlighting
+            	;;
+        *)
+            	usage
+            	;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [ -z "${p}" ]; then
+    usage
+fi
+

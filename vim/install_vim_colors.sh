859c9f38eb1499f62c6950a3269be32d53e429fe
Alexander Grooff <alexandergrooff@gmail.com>
2018-05-23 09:31:02 +0200

    Fix copy command

--- vim/install_vim_colors.sh
+++ vim/install_vim_colors.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # Move colors to vim config directory
 mkdir -p ~/.vim/colors
-cp colors ~/.vim/colors
+cp -R colors ~/.vim
 
 # Move plugins to vim config directory
 mkdir -p ~/.vim/plugins
-cp plugins ~/.vim/plugins
+cp -R plugins ~/.vim
 cat .vimrc > ~/.vimrc

fcbda3d05fb80a8baa0f26ca9e1adac834fbc327
Alexander Grooff <alexandergrooff@gmail.com>
2018-05-23 09:17:26 +0200

    Added prolog.vim and categorized plugins and colors

--- vim/install_vim_colors.sh
+++ vim/install_vim_colors.sh
@@ -1,3 +1,9 @@
+#!/usr/bin/env bash
+# Move colors to vim config directory
 mkdir -p ~/.vim/colors
-cp solarized.vim ~/.vim/colors/
+cp colors ~/.vim/colors
+
+# Move plugins to vim config directory
+mkdir -p ~/.vim/plugins
+cp plugins ~/.vim/plugins
 cat .vimrc > ~/.vimrc
